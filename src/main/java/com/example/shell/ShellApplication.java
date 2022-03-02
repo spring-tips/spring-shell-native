5611e84d7030d87f0dfd6723fa2b08ef78cc6dae
Josh Long <josh@joshlong.com>
2022-03-02 02:20:31 -0800

    shell ftw

--- src/main/java/com/example/shell/ShellApplication.java
+++ src/main/java/com/example/shell/ShellApplication.java
@@ -1,11 +1,16 @@
 package com.example.shell;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ShellApplication {
@@ -15,10 +20,45 @@ public class ShellApplication {
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    public void begin() {
-        System.out.println("I'm ready to help!");
+    public void beginInShellApplication() {
+        System.out.println("EL in Main");
     }
+}
 
+@Component
+class EL {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void begin() {
+        System.out.println("EL");
+    }
+}
+
+@Component
+class AL implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        System.out.println("AL");
+    }
+}
+
+@Component
+class CLR implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CLR");
+    }
+}
+
+@Component
+class AR implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("AR");
+    }
 }
 
 @ShellComponent
